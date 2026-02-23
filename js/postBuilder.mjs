64c5565fba44de3145d79e63d6f261f9ee1133fe
RMB <mosquedabruben@gmail.com>
2026-02-23 12:11:39 -0600

    Wip: creador de post. Se agregaron los To Do en post builder.mjs

--- js/postBuilder.mjs
+++ js/postBuilder.mjs
@@ -1,3 +1,32 @@
 "use strict";
 
-import { postGaleryDB } from "./postGaleryDB.mjs";
+import { articlesCodePenDB } from "./postGaleryDB.mjs";
+import { articlesDesingDB } from "./postGaleryDB.mjs";
+
+//TODO: Crear la funcion que genere la structura HTML para cada post de la seccion de CodePen
+//**Codepen post generator */
+
+/* hmtl Codepen post
+  <div class="swiper-slide">
+    <a href="Link" target="_blank" class="designCard designImgDefault" >
+      <div class="designInfo">
+        <h2>My Behance Space</h2> 
+        <h3>Rubén Mosqueda</h3>
+      </div>
+    </a>
+  </div>
+*/
+
+//TODO: Crear la funcion que genere la structura HTML para cada post de la seccion de Diseños
+//**Diseños post generator */
+
+/* html Diseñós post
+  <div class="swiper-slide">
+    <button id="myBtn" class="designCard designImg_6">
+      <div class="designInfo">
+        <h2>Pusheen Skin Twitch Panels</h2> 
+        <h3>Rubén Mosqueda</h3>
+      </div>
+    </button> 
+  </div>
+*/
