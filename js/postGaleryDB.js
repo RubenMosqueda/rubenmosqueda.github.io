af66cc35ffd896f74df2e09230316180dfe771a7
RMB <mosquedabruben@gmail.com>
2026-02-23 11:46:49 -0600

    Wip: creador de post. Se crearon los arcchivos .js de base de datos y creador de post

---
+++ js/postGaleryDB.js
@@ -0,0 +1,31 @@
+"use strict";
+
+export const articlesCodePenDB =
+[
+    {
+      image:"set image link",
+      logo:"set logo link",
+      title:"set post title",
+      autor:"Rubén Mosqueda"
+    },
+    {
+      image:"set image link",
+      logo:"set logo link",
+      title:"set post title",
+      autor:"Rubén Mosqueda"
+    }
+]
+
+export const articlesDesingDB =
+[
+    {
+      image:"set image link",
+      title:"set post title",
+      autor:"Rubén Mosqueda"
+    },
+    {
+      image:"set image link",
+      title:"set post title",
+      autor:"Rubén Mosqueda"
+    }
+]
