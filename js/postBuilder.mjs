9b2758835581ef37c756184bbdb0de7a37148cad
RMB <mosquedabruben@gmail.com>
2026-02-23 16:48:47 -0600

    Wip: creador de post. script codepen post creator done. actualizacon BD codepen en postgaleryDB done. se elminaron los liks de imagenes de codepen del css

--- js/postBuilder.mjs
+++ js/postBuilder.mjs
@@ -2,20 +2,68 @@
 
 import { articlesCodePenDB } from "./postGaleryDB.mjs";
 import { articlesDesingDB } from "./postGaleryDB.mjs";
+import { logo } from "./postGaleryDB.mjs";
 
 //TODO: Crear la funcion que genere la structura HTML para cada post de la seccion de CodePen
 //**Codepen post generator */
 
 /* hmtl Codepen post
   <div class="swiper-slide">
-    <a href="Link" target="_blank" class="designCard designImgDefault" >
-      <div class="designInfo">
-        <h2>My Behance Space</h2> 
-        <h3>Rubén Mosqueda</h3>
+    <a class="codepenCard" href="https://codepen.io/rubenmosqueda/pen/zYWPoVQ" target="_blank">
+      <div class="codepenImg cpImage1"></div>
+      <div class="codepenBackground"></div>
+      <div class="codepenDescription">
+        <div class="codepenProfileImage"></div>
+        <div class="codepenInfo">
+          <h2>CSS Sprite Animation</h2>
+          <h3>Rubén Mosqueda Bautista</h3>
+        </div>
       </div>
     </a>
   </div>
 */
+
+export function CodePenSlideBuilder(indexPost, mySwiper){
+  /*creamos el div que contendra el post que queremos crear */
+  const codePenslide = document.createElement("div");
+  /*Le asignamos la clase slide*/
+  codePenslide.className += ("swiper-slide");
+  /*Colocamos todo el HTML que ira dentro de este bloque*/
+  /*Esta slide en particular contendra 5 variables que seran leidas de la base de datos de postGaleryDB.jms*/
+  /*
+  ${articlesCodePenDB[indexPost].codePenLink}
+  ${articlesCodePenDB[indexPost].image}
+  ${logo}
+  ${articlesCodePenDB[indexPost].title}
+  ${articlesCodePenDB[indexPost].autor}
+  */
+  codePenslide.innerHTML = `
+    <a class="codepenCard" href="${articlesCodePenDB[indexPost].codePenLink}" target="_blank">
+      <div class="codepenImg" style="background-image: url('${articlesCodePenDB[indexPost].image}');"></div>
+      <div class="codepenBackground"></div>
+      <div class="codepenDescription">
+        <div class="codepenProfileImage" style="background-image: url('${logo}');"></div>
+        <div class="codepenInfo">
+          <h2>${articlesCodePenDB[indexPost].title}</h2>
+          <h3>${articlesCodePenDB[indexPost].autor}</h3>
+        </div>
+      </div>
+    </a>
+  `;
+  mySwiper.appendChild(codePenslide);
+}
+
+/*Funcion que rellena la seccion  Swiper-wrapper con los post creados por CodePenSlideBuilder() */
+export function codePenSlideFiller() {
+  const mySwiper = document.getElementById("swiper-wrapper");
+  for (var indexPost = 0; indexPost < articlesCodePenDB.length; indexPost++) {
+    CodePenSlideBuilder(indexPost, mySwiper);
+  }
+}
+
+/*Llamamos a la funcion para que rellene el swiper con todos los post de CodePen */
+codePenSlideFiller();
+
 
 //TODO: Crear la funcion que genere la structura HTML para cada post de la seccion de Diseños
 //**Diseños post generator */
--- js/postGaleryDB.mjs
+++ js/postGaleryDB.mjs
@@ -1,17 +1,72 @@
 "use strict";
 
+export const logo = "../imagenes/Logo.png";
 export const articlesCodePenDB =
 [
     {
-      image:"set image link",
-      logo:"set logo link",
-      title:"set post title",
+      codePenLink:"https://codepen.io/rubenmosqueda",
+      image:"../imagenes/codepen_my_codepen_space.jpg",
+      title:"My CodePen Space",
       autor:"Rubén Mosqueda"
     },
     {
-      image:"set image link",
-      logo:"set logo link",
-      title:"set post title",
+      codePenLink:"https://codepen.io/rubenmosqueda/pen/jOpwEBZ",
+      image:"../imagenes/codepen_layout_responsive.jpg",
+      title:"Layout Responsive",
+      autor:"Rubén Mosqueda"
+    },
+    {
+      codePenLink:"https://codepen.io/rubenmosqueda/pen/QWBpzzY",
+      image:"../imagenes/codepen_accordion_list_responsive_with_javascript.jpg",
+      title:"Accordion List Responsive with JavaScript",
+      autor:"Rubén Mosqueda"
+    },
+    {
+      codePenLink:"https://codepen.io/rubenmosqueda/pen/jOpMJMp",
+      image:"../imagenes/codepen_accordion_list_with_transition_css.jpg",
+      title:"Accordion List With Transition CSS",
+      autor:"Rubén Mosqueda"
+    },
+    {
+      codePenLink:"https://codepen.io/rubenmosqueda/pen/oNMzaWO",
+      image:"../imagenes/codepen_accordion_list_css.JPG",
+      title:"Accordion List CSS",
+      autor:"Rubén Mosqueda"
+    },
+    {
+      codePenLink:"https://codepen.io/rubenmosqueda/pen/RwJOVGp",
+      image:"../imagenes/codepen_e_book_card.JPG",
+      title:"e-Book Responsive Card",
+      autor:"Rubén Mosqueda"
+    },
+    {
+      codePenLink:"https://codepen.io/rubenmosqueda/pen/ZERZpvR",
+      image:"../imagenes/codepen_fake_window_card.JPG",
+      title:"Window Card",
+      autor:"Rubén Mosqueda"
+    },
+    {
+      codePenLink:"https://codepen.io/rubenmosqueda/pen/NWzJEXq",
+      image:"../imagenes/codepen_behance_card.JPG",
+      title:"Behance Card",
+      autor:"Rubén Mosqueda"
+    },
+    {
+      codePenLink:"https://codepen.io/rubenmosqueda/pen/WNymygW",
+      image:"../imagenes/codepen_card.JPG",
+      title:"Codepen Card",
+      autor:"Rubén Mosqueda"
+    },
+    {
+      codePenLink:"https://codepen.io/rubenmosqueda/pen/WNyopYK",
+      image:"../imagenes/codepen_landing_page_exercise.JPG",
+      title:"Personal Landing Page Exercise",
+      autor:"Rubén Mosqueda"
+    },
+    {
+      codePenLink:"https://codepen.io/rubenmosqueda/pen/zYWPoVQ",
+      image:"../imagenes/codepen_css_sprite_animation.JPG",
+      title:"CSS Sprite Animation",
       autor:"Rubén Mosqueda"
     }
 ]
