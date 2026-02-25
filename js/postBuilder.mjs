4f307ee549b5031d623c57f00ea50a65d46ddaef
RMB <mosquedabruben@gmail.com>
2026-02-25 13:21:07 -0600

    Wip limpieza de codig. postbuilder.mjs punto de control_1.

--- js/postBuilder.mjs
+++ js/postBuilder.mjs
@@ -4,39 +4,36 @@ import { articlesCodePenDB } from "./postGaleryDB.mjs";
 import { articlesDesingDB } from "./postGaleryDB.mjs";
 import { logo } from "./postGaleryDB.mjs";
 
-//TODO: Crear la funcion que genere la structura HTML para cada post de la seccion de CodePen
-//**Codepen post generator */
-
+//*<--------------------Codepen Slide Builder ---------------------->*/
 /* hmtl Codepen post
   <div class="swiper-slide">
-    <a class="codepenCard" href="https://codepen.io/rubenmosqueda/pen/zYWPoVQ" target="_blank">
-      <div class="codepenImg cpImage1"></div>
+    <a class="codepenCard" href="Set Link" target="_blank">
+      <div class="codepenImg" style="background-image: url('Set Image URL');"></div>
       <div class="codepenBackground"></div>
       <div class="codepenDescription">
-        <div class="codepenProfileImage"></div>
+        <div class="codepenProfileImage" style="background-image: url('Set Logo URL);"></div>
         <div class="codepenInfo">
-          <h2>CSS Sprite Animation</h2>
-          <h3>Rubén Mosqueda Bautista</h3>
+          <h2>"Set Title"</h2>
+          <h3>"Set Author"</h3>
         </div>
       </div>
     </a>
   </div>
 */
 
+//*? Parametros de entrada: numero de post, Swiper donde ira la slide*/
 export function CodePenSlideBuilder(indexPost, mySwiper){
-  /*creamos el div que contendra el post que queremos crear */
+  //*? Creamos el elemento que contendra el post/
   const codePenslide = document.createElement("div");
-  /*Le asignamos la clase slide*/
+  //*? Le asignamos la clase slide*/
   codePenslide.className += ("swiper-slide");
-  /*Colocamos todo el HTML que ira dentro de este bloque*/
-  /*Esta slide en particular contendra 5 variables que seran leidas de la base de datos de postGaleryDB.jms*/
-  /*
-  ${articlesCodePenDB[indexPost].codePenLink}
-  ${articlesCodePenDB[indexPost].image}
-  ${logo}
-  ${articlesCodePenDB[indexPost].title}
-  ${articlesCodePenDB[indexPost].autor}
-  */
+  /* Esta slide contendra 5 variables que seran leidas de la base de datos de postGaleryDB.jms 
+   1- ${articlesCodePenDB[indexPost].codePenLink} 
+   2- ${articlesCodePenDB[indexPost].image} 
+   3- ${logo} 
+   4- ${articlesCodePenDB[indexPost].title} 
+   5- ${articlesCodePenDB[indexPost].autor} */
+  //*? Colocamos todo el HTML que ira dentro de este bloque*/
   codePenslide.innerHTML = `
     <a class="codepenCard" href="${articlesCodePenDB[indexPost].codePenLink}" target="_blank">
       <div class="codepenImg" style="background-image: url('${articlesCodePenDB[indexPost].image}');"></div>
@@ -50,11 +47,11 @@ export function CodePenSlideBuilder(indexPost, mySwiper){
       </div>
     </a>
   `;
+  //*? Agregamos el post creado al bloque swiper deseado (codepenSwiper)*/
   mySwiper.appendChild(codePenslide);
 }
 
-//TODO: Crear la funcion que genere la structura HTML para cada post de la seccion de Diseños
-//**Diseños post generator */
+//*<---------------------Design Slide Builder ---------------------->*/
 
 /* html Diseños post
   <div class="swiper-slide">
@@ -66,18 +63,17 @@ export function CodePenSlideBuilder(indexPost, mySwiper){
     </button> 
   </div>
 */
+
 export function DesingSlideBuilder(indexPost, mySwiper){
-  /*creamos el div que contendra el post que queremos crear */
+  //*? Creamos el elemento que contendra el post/
   const desingSlide = document.createElement("div");
-  /*Le asignamos la clase slide*/
+  //*? Le asignamos la clase slide*/
   desingSlide.className += ("swiper-slide");
-  /*Colocamos todo el HTML que ira dentro de este bloque*/
-  /*Esta slide en particular contendra 3 variables que seran leidas de la base de datos de postGaleryDB.jms*/
-  /*
-  ${articlesDesingDB[indexPost].image}
-  ${articlesDesingDB[indexPost].title}
-  ${articlesDesingDB[indexPost].autor}
-  */
+  /* Esta slide contendra 3 variables que seran leidas de la base de datos de postGaleryDB.jms 
+   1- ${articlesDesingDB[indexPost].image}
+   2- ${articlesDesingDB[indexPost].title}
+   3- ${articlesDesingDB[indexPost].autor}*/
+  //*? Colocamos todo el HTML que ira dentro de este bloque*/
   desingSlide.innerHTML = `
     <button id="btn_${indexPost}" class="designCard btn" style="background-image: url('${articlesDesingDB[indexPost].minature}');">
       <div class="designInfo">
@@ -86,25 +82,37 @@ export function DesingSlideBuilder(indexPost, mySwiper){
       </div>
     </button> 
   `;
+  //*? Agregamos el post creado al bloque swiper deseado (designSwiper)*/
   mySwiper.appendChild(desingSlide);
 }
 
-/*Funcion que rellena la seccion  Swiper-wrapper con los post creados por CodePenSlideBuilder() */
+//*<--------------------Function Swiper Filler --------------------->*/
+
+//*? Parametros de entrada: Swiper a rellenar*/
 export function postSlideFiller(swiperWeapper) {
+  
+  //*? Seleccionamos el swiper segun el Id del swiper de entrada (swiperWeapper)*/
   const mySwiper = document.getElementById(swiperWeapper);
+  //*? Declaramos un arreglo de botones vacio */
   let btnArray = [];
+
+  //*? Selector de tipo de Slide a rellenar */
   if(swiperWeapper == "swiper-wrapper-cp"){
+    //**? Ciclo para rellenar el Swiper tipo CODEPEN */
     for (var indexPost = 0; indexPost < articlesCodePenDB.length; indexPost++) {
     CodePenSlideBuilder(indexPost, mySwiper);
     }
   }
   if(swiperWeapper == "swiper-wrapper-dsg"){
+    //**? Ciclo para rellenar el Swiper tipo DISEÑOS */
     for (var indexPost = 0; indexPost < articlesDesingDB.length; indexPost++) {
     DesingSlideBuilder(indexPost, mySwiper);
     /*btnArray.push( 'btn_' + indexPost );
       console.log(btnArray);*/
     } 
   } 
+
+  
   // Selecciona todos los botones con la clase 'btn'
   const botones = document.querySelectorAll(".btn");
   console.log(botones);
@@ -131,7 +139,7 @@ export function postSlideFiller(swiperWeapper) {
         console.log(articlesDesingDB[index].image);
 
 
-      // When the user clicks anywhere outside of the modal, close it
+      // When the user clicks anywhere outside of the moda, close it
       window.onclick = function(event) {
         if (event.target == modal_galery ||event.target == modal) {
           modal.style.display = "none";
