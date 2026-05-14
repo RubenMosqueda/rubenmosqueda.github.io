0b4351efab2e45fd8d4cfb02afdbe88cbba7f3c9
RMB <mosquedabruben@gmail.com>
2026-05-14 08:46:39 -0600

    Punto de control 1. Se crearon archivos provisionales para la interface y se comenzo a trabajar en ella. (nota override)

--- js/positionNav.js
+++ js/positionNav.js
@@ -4,22 +4,33 @@ window.addEventListener('resize', fixedBehavior);
 function fixedBehavior(){
 
   const windowWidth = window.innerWidth;
+  const windowHeigth = window.innerHeight;
   const contenidoWidth  = document.documentElement.scrollWidth;
 
   const navbar = document.getElementById("navbar");
+  const navbarHeigth = navbar.offsetHeight;
 
-  if (windowWidth < 1000){
-    navbar.style.right = "0px";
+  const contentBlock = document.getElementById("content");
+  const contentHeigth = contentBlock.offsetHeight;
+
+  console.log("altura de nav " + navbarHeigth);
+  const topPosition =  (contentHeigth / 2) - (navbarHeigth / 2);
+  
+  navbar.style.top = topPosition + "px";
+
+  if (windowWidth < 1600){
+    navbar.style.right = "0.5rem";
   }
 
-  if(windowWidth > 1000){
+  if(windowWidth > 1600){
 
     if(contenidoWidth<windowWidth){
-      var rightPosition = ((windowWidth-1000)/2) - 9;
+      var rightPosition = ((windowWidth-1600)/2) - 9;
       navbar.style.right = rightPosition + "px";
+
     }
     else{
-      var rightPosition = (windowWidth-1000)/2;
+      var rightPosition = ((windowWidth-1600)/2) + 9;
       navbar.style.right = rightPosition + "px";
     }
     
